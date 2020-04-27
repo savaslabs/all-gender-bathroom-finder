1c3f5b1502d48eab968b87081e3eb6b68f435293
Alex Manzo <manzoalex17@gmail.com>
2020-04-27 15:05:15 -0400

    Create Add New Form.

--- src/components/add/AddNewForm.js
+++ src/components/add/AddNewForm.js
@@ -1,1 +1,76 @@
-// @todo: Write component based on current code within /views/AddNew
+/**
+ * @file WORK IN-PROGRESS
+ *
+ * Add new location form.
+ *
+ * Form for users adding new location.
+ *
+ */
+
+import React, { useContext, useEffect } from 'react';
+import { Button, Text, View, Switch } from 'react-native';
+import { withNavigation } from 'react-navigation';
+
+import { PlaceContext } from '../../context/PlaceContext';
+import {
+  getBathroomTypes,
+  setLocationInFirestore,
+} from '../../services/firestore-service';
+
+import PlacesAutocompleteInput from '../search/PlacesAutocompleteInput';
+import PlacePreview from './PlacePreview';
+import BathroomTypePicker from './BathroomTypePicker';
+
+export function AddNewForm({ navigation: { navigate } }) {
+  const Place = useContext(PlaceContext);
+
+  useEffect(() => {
+    getBathroomTypes().then((bathroomList) => {
+      Place.setBathroomTypes(bathroomList);
+    });
+  }, []);
+
+  const onPlaceSelect = (place) => {
+    Place.setPreview(`${place.result.name}\n${place.result.vicinity}`);
+    Place.createLocationFromGooglePlace(place);
+  };
+
+  const handleSubmit = () => {
+    setLocationInFirestore(Place.placeId, Place.newPlace);
+    Place.resetPlace();
+    navigate('AddSuccess');
+  };
+
+  return (
+    <View>
+      <PlacesAutocompleteInput
+        handleSelection={(place) => onPlaceSelect(place)}
+        queryFields={
+          'place_id,name,vicinity,formatted_address,formatted_phone_number,types,website'
+        }
+        queryTypes={'establishment'}
+      />
+      <PlacePreview />
+      <Text>What type of bathroom is at this location?</Text>
+      <BathroomTypePicker />
+      <Text>Is this bathroom accessible?</Text>
+      <Switch
+        value={Place.newPlace.accessible}
+        onValueChange={(value) =>
+          Place.setNewPlace({ ...Place.newPlace, accessible: value })
+        }
+      ></Switch>
+      <Text>Does this bathroom have a changing table available?</Text>
+      <Switch
+        value={Place.newPlace.changing_table}
+        onValueChange={(value) =>
+          Place.setNewPlace({ ...Place.newPlace, changing_table: value })
+        }
+      ></Switch>
+      <Button title="Add Location" onPress={() => handleSubmit()} />
+      <Button title="Start over" onPress={() => Place.resetPlace()} />
+    </View>
+  );
+}
+
+export default withNavigation(AddNewForm);
--- src/views/AddNew.js
+++ src/views/AddNew.js
@@ -1,12 +1,6 @@
 /**
  * @file WORK IN-PROGRESS
  *
- * @todo
- * - Break down into smaller components
- * - Move logic either into component or firebase files.
- * - Conditional display for the Picker
- * - Handle success/error on posting to Firebase.
- *
  * Add new location page.
  *
  * User can use the autocomplete input to search for a
@@ -16,71 +10,19 @@
  *
  */
 
-import React, { useContext, useEffect } from 'react';
-import { Button, Text, View, Picker, Switch } from 'react-native';
-import PlacesAutocompleteInput from '../components/search/PlacesAutocompleteInput';
+import React, { useContext } from 'react';
+import { Button, Text, View } from 'react-native';
 import { AuthContext } from '../context/AuthContext';
-import { PlaceContext } from '../context/PlaceContext';
-import {
-  getBathroomTypes,
-  setLocationInFirestore,
-} from '../services/firestore-service';
 
-import PlacePreview from '../components/add/PlacePreview';
-import BathroomTypePicker from '../components/add/BathroomTypePicker';
+import AddNewForm from '../components/add/AddNewForm';
 
 export default function AddNew({ navigation: { navigate } }) {
   const Auth = useContext(AuthContext);
-  const Place = useContext(PlaceContext);
-
-  useEffect(() => {
-    getBathroomTypes().then((bathroomList) => {
-      Place.setBathroomTypes(bathroomList);
-    });
-  }, []);
-
-  const onPlaceSelect = (place) => {
-    Place.setPreview(`${place.result.name}\n${place.result.vicinity}`);
-    Place.createLocationFromGooglePlace(place);
-  };
-
-  const handleSubmit = () => {
-    setLocationInFirestore(Place.placeId, Place.newPlace);
-    Place.resetPlace();
-    navigate('AddSuccess');
-  };
 
   return (
     <View style={{ marginTop: 50 }}>
       {Auth.isLoggedIn ? (
-        <View>
-          <PlacesAutocompleteInput
-            handleSelection={(place) => onPlaceSelect(place)}
-            queryFields={
-              'place_id,name,vicinity,formatted_address,formatted_phone_number,types,website'
-            }
-            queryTypes={'establishment'}
-          />
-          <PlacePreview />
-          <Text>What type of bathroom is at this location?</Text>
-          <BathroomTypePicker />
-          <Text>Is this bathroom accessible?</Text>
-          <Switch
-            value={Place.newPlace.accessible}
-            onValueChange={(value) =>
-              Place.setNewPlace({ ...Place.newPlace, accessible: value })
-            }
-          ></Switch>
-          <Text>Does this bathroom have a changing table available?</Text>
-          <Switch
-            value={Place.newPlace.changing_table}
-            onValueChange={(value) =>
-              Place.setNewPlace({ ...Place.newPlace, changing_table: value })
-            }
-          ></Switch>
-          <Button title="Add Location" onPress={() => handleSubmit()} />
-          <Button title="Start over" onPress={() => Place.resetPlace()} />
-        </View>
+        <AddNewForm />
       ) : (
         <Text>
           Please <Button title="log in" onPress={() => navigate('Login')} /> or{' '}
